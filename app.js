547d174df9d71967c601f0f86392b0f56efe4bb6
Allison Livingston <livingstona16@gmail.com>
2018-08-28 21:22:10 -0600

    Adding progress of all sections and mobile views

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-let welcome = document.querySelector('.home-nav')
-let main = document.querySelector('.home-main')
-let name = document.querySelector('.index-header')
+// -- IMAGE CAROUSEL -- //
 
-name.addEventListener("click", function(event) {
-  event.preventDefault()
-  welcome.style.display = 'flex'
-  main.style.height = '40vh'
-  welcome.scrollIntoView({
-  behavior: 'smooth'
-  })
-})
+var myIndex = 0
+carousel()
 
-var modal = document.getElementById('myModal')
-
-var img = document.getElementById('myImg')
-var modalImg = document.getElementById("img01")
-var captionText = document.getElementById("caption")
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = Resume-Allison-Livingston.png;
+function carousel() {
+    var i
+    var x = document.getElementsByClassName('myImages')
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = 'none'
+    }
+    myIndex++
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = 'block'
+    setTimeout(carousel, 8500)
 }
 
-var span = document.getElementsByClassName("close")[0];
+// -- STICKY NAV BAR -- //
 
-span.onclick = function() {
-  modal.style.display = "none"
+const nav = document.querySelector('#navigation')
+const navTop = nav.offsetTop
+
+function stickyNav() {
+  if(window.scrollY >= navTop) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px'
+    document.body.classList.add('fixed-header')
+  } else {
+    document.body.style.paddingTop = 0
+    document.body.classList.remove('fixed-header')
+  }
 }
+
+window.addEventListener('scroll', stickyNav)
